0feca7cbde81781341241c0eb2d4dab444365450
Rory Quinlan <72473095+RoryQo@users.noreply.github.com>
2025-05-11 19:09:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
 from setuptools import setup, find_packages
 
 setup(
     name='data_buyer_toolkit',
     version='0.1.0',
     author='Rory G. Quinlan',
-    description='Toolkit for scoring public sector job postings for third-party data demand.',
+    description='Toolkit for scoring USA Jobs open position postings for third-party data demand.',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    packages=find_packages(),  # <--- THIS automatically finds /data_buyer_toolkit/
+    packages=find_packages(),  # Finds /data_buyer_toolkit/
+    include_package_data=True,  # <--- tell setuptools to look for extra files
+    package_data={              # <--- explicitly list the joblib files
+        'data_buyer_toolkit': [
+            'nlp_model.joblib',
+            'nlp_pipeline_with_smote.joblib',
+            'vectorizer.joblib'
+        ]
+    },
     install_requires=[
         'pandas',
         'numpy',
@@ -29,5 +35,3 @@ setup(
     ],
     python_requires='>=3.8',
 )
-
-
