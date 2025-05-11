738d6457e484c5d03443db811a5284846ba34181
Rory Quinlan <72473095+RoryQo@users.noreply.github.com>
2025-05-11 10:49:33 -0400

    Update  __init__.py

--- data_buyer_toolkit/ __init__.py
+++ data_buyer_toolkit/ __init__.py
@@ -6,5 +6,4 @@
 
 from .fetch_and_score import fetch_and_score_job, batch_fetch_and_score_jobs
 from .preprocess_job import preprocess_job_api_response
-from .search_and_score import search_and_score_keyword_live
-
+from .search_and_score import search_job_ids_by_title
