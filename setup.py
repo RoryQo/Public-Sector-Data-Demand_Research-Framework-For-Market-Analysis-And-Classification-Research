79d054187968065a9d2d0e5f505cd3b2b34d12e1
Rory Quinlan <72473095+RoryQo@users.noreply.github.com>
2025-05-11 15:28:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,32 +3,31 @@
 
 # In[ ]:
 
-
 from setuptools import setup, find_packages
 
 setup(
     name='data_buyer_toolkit',
-    version='0.1',
-    packages=find_packages(where='Automated Data and Lead Generators/Data Buyer Tool Kit Package'),
-    package_dir={'': 'Automated Data and Lead Generators/Data Buyer Tool Kit Package'},
+    version='0.1.0',
+    author='Rory G. Quinlan',
+    description='Toolkit for scoring public sector job postings for third-party data demand.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),  # <--- THIS automatically finds /data_buyer_toolkit/
     install_requires=[
         'pandas',
         'numpy',
         'scikit-learn',
-        'requests',
         'nltk',
-        'rapidfuzz'
+        'requests',
+        'rapidfuzz',
+        'joblib'
     ],
-    author='Rory G. Quinlan',
-    description='Toolkit for scoring third-party data buyer demand in USAJobs postings',
-    long_description=open('README.md', encoding='utf-8').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/RoryQo/Public-Sector-Data-Demand_Research-Framework-For-Market-Analysis-And-Classification',
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License'
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7'
+    python_requires='>=3.8',
 )
 
 
