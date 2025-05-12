c0dde5f56d2696f09572d5042f85d325e6f42f1e
Rory Quinlan <72473095+RoryQo@users.noreply.github.com>
2025-05-11 22:06:51 -0400

    Update  __init__.py

--- data_buyer_toolkit/ __init__.py
+++ data_buyer_toolkit/ __init__.py
@@ -12,7 +12,6 @@ from .toolkit import (
     batch_fetch_and_score_jobs,
     search_job_ids_by_title,
     fetch_and_score_top_by_use_case_auto,
-    fetch_and_score_top_by_custom_inputs,
     preprocess_job_api_response,
     load_pipeline,
 )
