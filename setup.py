a8716468927701cbc22837bd448c74c65a0a894b
Rory Quinlan <72473095+RoryQo@users.noreply.github.com>
2025-05-11 20:16:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 from setuptools import setup, find_packages
 
 setup(
     name='data_buyer_toolkit',
     version='0.1.0',
     author='Rory G. Quinlan',
-    description='Toolkit for scoring USA Jobs available job postings for third-party data demand.',
+    description='Toolkit for identifying third-party data buyers in U.S. federal job postings from USAJobs.',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    include_package_data=True,  # <-- ADD THIS
+    include_package_data=True,
     package_data={
-        'data_buyer_toolkit': ['*.joblib'],  # <-- ADD THIS to tell it to ship the .joblib files
+        'data_buyer_toolkit': ['*.joblib'],
     },
     install_requires=[
         'pandas',
@@ -22,9 +25,9 @@ setup(
         'joblib'
     ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.8',
 )
