0bda1dc2a88f06de4458240932e371414b024a0e
Rory Quinlan <72473095+RoryQo@users.noreply.github.com>
2025-05-13 15:24:14 -0400

    Update  __init__.py

--- data_buyer_toolkit/ __init__.py
+++ data_buyer_toolkit/ __init__.py
@@ -8,10 +8,13 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 
 # --- Re-export main functions ---
 from .toolkit import (
-    fetch_and_score_job,
-    batch_fetch_and_score_jobs,
-    search_job_ids_by_title,
-    fetch_and_score_top_by_use_case_auto,
-    preprocess_job_api_response,
     load_pipeline,
+    preprocess_job_api_response,
+    fetch_and_score_job,
+    search_job_ids_by_title,
+    batch_fetch_and_score_jobs,
+    fetch_and_score_top_by_use_case_auto,
+    fetch_top_data_buyers_by_industry_auto,
+    fetch_and_score_top_by_use_case_custom,
+    fetch_top_data_buyers_by_industry_custom,
 )
